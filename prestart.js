ec19fc3862effc5ce92a6801e4507f947b39434c
ATD1979 <atdruba@gmail.com>
2026-04-12 20:14:18 -0500

    Create prestart.js

---
+++ prestart.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+console.log('PRESTART RUNNING');
+const lines = fs.readFileSync('public/index.html', 'utf8').split('\n');
+const i = lines.findIndex(l => l.length > 100000);
+if (i !== -1) {
+  lines.splice(i, 1);
+  fs.writeFileSync('public/index.html', lines.join('\n'));
+  console.log('Blob removed at line ' + (i + 1));
+} else {
+  console.log('No blob found - index.html is clean');
+}
