5505e8edbf3c5153e0e2d469900f035d5c934a76
ATD1979 <atdruba@gmail.com>
2026-04-13 15:56:47 -0500

    Update prestart.js

--- prestart.js
+++ prestart.js
@@ -1,10 +1,15 @@
 const fs = require('fs');
 console.log('PRESTART RUNNING');
-const lines = fs.readFileSync('public/index.html', 'utf8').split('\n');
+const path = 'public/index.html';
+if (!fs.existsSync(path)) {
+  console.log('index.html not found - skipping blob check');
+  process.exit(0);
+}
+const lines = fs.readFileSync(path, 'utf8').split('\n');
 const i = lines.findIndex(l => l.length > 500000);
 if (i !== -1) {
   lines.splice(i, 1);
-  fs.writeFileSync('public/index.html', lines.join('\n'));
+  fs.writeFileSync(path, lines.join('\n'));
   console.log('Blob removed at line ' + (i + 1));
 } else {
   console.log('No blob found - index.html is clean');
